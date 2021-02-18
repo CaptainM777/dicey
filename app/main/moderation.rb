7bdb48fd00aa849c26fdd87a0469630ddc604fcd
Cap <backmanman@gmail.com>
2021-02-18 16:47:10 -0500

    Warn command: this command can only be used by users who have either the 'Admin' role or the 'Moderator' role | Added 'ADMIN_ROLE_ID' and 'STAFF_ROLES' to 'constants.rb'

--- app/main/moderation.rb
+++ app/main/moderation.rb
@@ -1,38 +1,21 @@
 # Crystal: Moderation - Write what the crystal does here.
+
+require 'rufus-scheduler'
+
 module Bot::Moderation
   extend Discordrb::Commands::CommandContainer
   extend Discordrb::EventContainer
   include Constants
 
   MOD_LOG ||= Bot::BOT.channel(MOD_LOG_ID)
-  # DM_MESSAGES = {
-  #   warning: "**⚠ You've received a warning from the server staff.**",
-  #   mute: "**🔇 You've been muted by the server staff.**",
-  #   perma_ban: "**You've been permanently banned from the server.**",
-  #   temp_ban: "**You've been temporarily banned from the server.**"
-  # }
-
-  # # Module methods
-  # class << self
-  #   def user_on_server?(user)
-  #     user.on(event.server)
-  #   end
-
-  #   def dm_user(user, command_type, )
-  #     begin
-  #       user
-  #   end
-  # end
 
   def self.valid_user?(user, channel)
     return Bot::BOT.get_user(user) if Bot::BOT.get_user(user)
     channel.send_temporary_message("**The user you provided is invalid! Make sure you give a valid user ID or user mention.**", 15)
   end
   
-  command :warn do |event, user="", *reason|
-    break unless event.user.has_permission?(:mod) && 
-                 (user = valid_user?(user, event.channel)) &&
-                 !reason.empty?
+  command :warn, allowed_roles: STAFF_ROLES do |event, user="", *reason|
+    break unless (user = valid_user?(user, event.channel)) && !reason.empty?
     dm_sent = false
     begin
       user.dm("**⚠ You've received a warning from the server staff.**\n**Reason:** #{reason.join(" ")}")
--- lib/constants.rb
+++ lib/constants.rb
@@ -5,9 +5,16 @@ module Constants
     ENV['BOT_NAME'] == "thunder" ? tox_id : personal_id
   end
 
-  CAP_ID = 260600155630338048
+  # Roles
+  ADMIN_ROLE_ID ||= determine_id(807691115049648158, 665696338536824848)
   MOD_ROLE_ID ||= determine_id(807694130448302080, 665696287772901416)
+  STAFF_ROLES ||= [ADMIN_ROLE_ID, MOD_ROLE_ID]
+
+  # Channels
   MOD_LOG_ID ||= determine_id(808921994540089385, 651969052155183128)
+
+  # Other constants
+  CAP_ID = 260600155630338048
   DB = Bot::DB
   SCHEDULER = Rufus::Scheduler.new
 end
