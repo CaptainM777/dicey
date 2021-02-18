0f44f70e13c30b6249c35fbfe8b43ab120d914fb
Cap <backmanman@gmail.com>
2021-02-17 21:51:30 -0500

    Warn command: now accounts for situations where a user either leaves the server, has DM's turned off for the server, or has blocked the bot.

--- app/main/moderation.rb
+++ app/main/moderation.rb
@@ -5,6 +5,24 @@ module Bot::Moderation
   include Constants
 
   MOD_LOG ||= Bot::BOT.channel(MOD_LOG_ID)
+  # DM_MESSAGES = {
+  #   warning: "**⚠ You've received a warning from the server staff.**",
+  #   mute: "**🔇 You've been muted by the server staff.**",
+  #   perma_ban: "**You've been permanently banned from the server.**",
+  #   temp_ban: "**You've been temporarily banned from the server.**"
+  # }
+
+  # # Module methods
+  # class << self
+  #   def user_on_server?(user)
+  #     user.on(event.server)
+  #   end
+
+  #   def dm_user(user, command_type, )
+  #     begin
+  #       user
+  #   end
+  # end
 
   def self.valid_user?(user, channel)
     return Bot::BOT.get_user(user) if Bot::BOT.get_user(user)
@@ -15,8 +33,21 @@ module Bot::Moderation
     break unless event.user.has_permission?(:mod) && 
                  (user = valid_user?(user, event.channel)) &&
                  !reason.empty?
-    # To-Do Task: account for scenarios where the bot attempts to DM someone not on the server
-    user.dm("**⚠ You've received a warning from the server staff.**\n**Reason:** #{reason.join(" ")}")
+    dm_sent = false
+    begin
+      user.dm("**⚠ You've received a warning from the server staff.**\n**Reason:** #{reason.join(" ")}")
+    rescue Discordrb::Errors::NoPermission
+      if user.on(event.server).nil?
+        event << "A DM couldn't be sent to this user because they left the server. This warning will not be logged."
+        break
+      end
+      event << "A DM couldn't be sent to this user because they either have DM's turned off for this server or have the bot blocked. " + 
+               "A warning will still be logged to the mod log channel."
+    else
+      event << "**Sent warning to #{user.distinct}.**"
+      dm_sent = true
+    end
+
     MOD_LOG.send_embed do |embed|
       embed.author = {
         name: "WARNING | #{user.name} (#{user.distinct})",
@@ -24,11 +55,11 @@ module Bot::Moderation
       }
       embed.description = "**⚠ #{user.mention} was given a warning by #{event.user.mention} (#{event.user.distinct})**"
       embed.add_field(name: "Reason", value: reason.join(" "))
+      embed.add_field(name: "Additional Information", value: "A DM couldn't be sent to this user.") unless dm_sent
       embed.footer = { 
         text: "User ID: #{user.id} | Staff ID: #{event.user.id} • #{Time.new.utc.strftime("%Y-%m-%d at %l:%M %p")}" 
       }
       embed.color = 0xFFD700
-      event << "**Sent warning to #{user.distinct}.**"
     end
   end
 end
